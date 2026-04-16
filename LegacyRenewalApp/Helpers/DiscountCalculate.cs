3ae8a0d71898aa37c8eb6c5beb03f504cbcde8bd
Julius Babelis <s31850@pjwst.edu.pl>
2026-04-16 12:51:18 +0200

    discount calculation

---
+++ LegacyRenewalApp/Helpers/DiscountCalculate.cs
@@ -0,0 +1,77 @@
+using LegacyRenewalApp.Interfaces;
+
+namespace LegacyRenewalApp;
+
+public class DiscountCalculate:IDiscountCalculate
+{
+    private decimal SubTotalDiscount;
+    
+    
+    public decimal CalculateDiscount(DiscountContext data)
+    {
+        
+        if (data.Customer.Segment == "Silver")
+            {
+                data.DiscountAmount += data.BaseAmount * 0.05m;
+                data.Notes.Add("silver discount;"); 
+            }
+            else if (data.Customer.Segment == "Gold")
+            {
+                data.DiscountAmount += data.BaseAmount * 0.10m;
+                data.Notes.Add("gold discount;");
+            }
+            else if (data.Customer.Segment == "Platinum")
+            {
+                data.DiscountAmount += data.BaseAmount * 0.15m;
+                data.Notes.Add("platinum discount; ");
+            }
+            else if (data.Customer.Segment == "Education" && data.Plan.IsEducationEligible)
+            {
+                data.DiscountAmount += data.BaseAmount * 0.20m;
+                data.Notes.Add("education discount; ");
+            }
+
+            if (data.Customer.YearsWithCompany >= 5)
+            {
+                data.DiscountAmount += data.BaseAmount * 0.07m;
+                data.Notes.Add("long-term loyalty discount; ");
+            }
+            else if (data.Customer.YearsWithCompany >= 2)
+            {
+                data.DiscountAmount += data.BaseAmount * 0.03m;
+                data.Notes.Add("basic loyalty discount; ");
+            }
+
+            if (data.SeatCount >= 50)
+            {
+                data.DiscountAmount += data.BaseAmount * 0.12m;
+                data.Notes.Add("large team discount; ");
+            }
+            else if (data.SeatCount >= 20)
+            {
+                data.DiscountAmount += data.BaseAmount * 0.08m;
+                data.Notes.Add("medium team discount; ");
+            }
+            else if (data.SeatCount >= 10)
+            {
+                data.DiscountAmount += data.BaseAmount * 0.04m;
+                data.Notes.Add("small team discount; ");
+            }
+
+            if (data.UseLoyaltyPoints && data.Customer.LoyaltyPoints > 0)
+            {
+                int pointsToUse = data.Customer.LoyaltyPoints > 200 ? 200 : data.Customer.LoyaltyPoints;
+                data.DiscountAmount += pointsToUse;
+                data.Notes.Add($"loyalty points used: {pointsToUse}; ");
+            }
+
+            decimal subtotalAfterDiscount = data.BaseAmount - data.DiscountAmount;
+            if (subtotalAfterDiscount < 300m)
+            {
+                subtotalAfterDiscount = 300m;
+                data.Notes.Add("minimum discounted subtotal applied; ");
+            }
+            
+            return data.DiscountAmount;
+    }
+}
---
+++ LegacyRenewalApp/Models/DiscountContext.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+namespace LegacyRenewalApp;
+
+public class DiscountContext
+{
+    public Customer Customer { get; set; }
+    public SubscriptionPlan Plan { get; set; }
+    public decimal BaseAmount { get; set; }
+    public bool UseLoyaltyPoints { get; set; }
+    public int SeatCount { get; set; }
+    
+    
+    
+    public decimal DiscountAmount { get; set; }
+    public List<string> Notes { get; set; } = new();
+}
